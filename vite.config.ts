4e401ba606472eec7f4af9d8c70cf3c15de90b04
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-09-13 01:01:10 +0600

    Change baseURL via environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000/",
+        target: process.env.baseURL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
